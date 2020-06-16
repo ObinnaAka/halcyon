b660e7bd7388c9a5ba34bf1269c4a020818302a8
ObinnaAka <33449354+ObinnaAka@users.noreply.github.com>
2020-06-16 04:59:57 -0500

    Added login screen and authentication. Can now register users.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
-import AuthPage from "./Pages/Auth";
+import AuthPage from "./Pages/Auth/Auth";
+import RegisterPage from "./Pages/Auth/Register";
 import StaffPortal from "./Pages/StaffPortal/StaffPortal";
 import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
-import AuthContext from "/context/auth-context";
+import AuthContext from "./context/auth-context";
 
 import "./App.css";
 
 function App() {
+	const [login, setLogin] = useState({ token: null, memberID: null });
+	const context = AuthContext;
+
+	const handleLogin = (token, memberID, tokenExpiration) => {
+		setLogin({
+			token: token,
+			memberID: memberID,
+		});
+	};
+	const handleLogout = () => {
+		setLogin({
+			token: null,
+			memberID: null,
+		});
+	};
 	return (
 		<BrowserRouter>
 			<React.Fragment>
-				<AuthContext.Provider>
+				<AuthContext.Provider
+					value={{
+						token: login.token,
+						memberID: login.memberID,
+						login: handleLogin,
+						logout: handleLogout,
+					}}
+				>
 					<NavBar />
 					<Switch>
-						<Redirect from="/" to="/auth" exact />
+						{context.token? <Redirect from="/" to="/staff" exact /> : <Redirect from="/" to="/auth" exact />}
 						<Route path="/auth" component={AuthPage} />
 						<Route path="/staff" component={StaffPortal} />
+						<Route path="/register" component={RegisterPage} />
 					</Switch>
 					<Footer />
 				</AuthContext.Provider>
---
+++ frontend/src/Pages/Auth/Register.js
@@ -0,0 +1,129 @@
+import React, { useContext, useState } from "react";
+import { Route, Redirect } from "react-router-dom";
+import AuthContext from "../../context/auth-context";
+import Button from "react-bootstrap/Button";
+import "./Auth.modules.css";
+import { useInput } from "../../helpers/useInputChange";
+import AuthPage from "./Auth";
+
+const RegisterPage = () => {
+	const {
+		value: firstName,
+		bind: bindFirstName,
+		reset: resetFirstName,
+	} = useInput("");
+	const {
+		value: lastName,
+		bind: bindLastName,
+		reset: resetLastName,
+	} = useInput("");
+	const { value: email, bind: bindEmail, reset: resetEmail } = useInput("");
+	const { value: eid, bind: bindEid, reset: resetEid } = useInput("");
+	const {
+		value: password,
+		bind: bindPassword,
+		reset: resetPassword,
+	} = useInput("");
+
+	// For student portal login
+	const [memberLogin, setMemberLogin] = useState(false);
+
+	const context = useContext(AuthContext);
+
+	const handleRegister = () => {
+		setMemberLogin(true);
+	};
+	const submitHandler = (evt) => {
+		evt.preventDefault();
+		if (eid.trim().length === 0 || password.trim().length === 0) {
+			alert("Invalid credentials entered");
+			return;
+		}
+		const requestBody = {
+			query: `
+            mutation {
+                createMember(memberInput: {firstName: "${firstName}", lastName: "${lastName}", email: "${email}", eid: "${eid}", password: "${password}", memberType:"Staff"}) {
+                  _id
+                  eid
+                }
+              }
+              
+			`,
+		};
+		fetch("http://localhost:8000/graphql", {
+			method: "POST",
+			body: JSON.stringify(requestBody),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+			.then((res) => {
+				if (res.status !== 200 && res.status !== 201) {
+					throw new Error("Failed");
+				}
+				return res.json();
+			})
+			.then((resData) => {
+				return <Redirect from="/register" to="/auth" />;
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+		// resetEid()
+		// resetPassword()
+	};
+
+	return (
+		<div>
+			{memberLogin && <Redirect from="/register" to="/auth" />}
+			{/* {context.token? <Redirect from="/" to="/staff" exact /> : <Redirect from="/" to="/auth" exact />} */}
+			<form className="page auth-app" onSubmit={submitHandler}>
+				<div className="form-control">
+					<label htmlFor="firstName">
+						First Name:
+						<input type="string" value={firstName} {...bindFirstName} />
+					</label>
+				</div>
+				<div className="form-control">
+					<label htmlFor="lastName">
+						Last Name:
+						<input type="string" value={lastName} {...bindLastName} />
+					</label>
+				</div>
+				<div className="form-control">
+					<label htmlFor="eid">
+						EID:
+						<input type="string" value={eid} {...bindEid} />
+					</label>
+				</div>
+				<div className="form-control">
+					<label htmlFor="email">
+						Email:
+						<input type="email" value={email} {...bindEmail} />
+					</label>
+				</div>
+				<div className="form-control">
+					<label htmlFor="password">
+						Password:
+						<input type="password" value={password} {...bindPassword} />
+					</label>
+				</div>
+
+				<div className="form-actions">
+					<input
+						type="button"
+						className="button"
+						value="Login"
+						onClick={handleRegister}
+					/>
+					<input type="submit" className="button" value="Register" />
+					{/* <Button type="submit" variant="primary">
+					Login
+				</Button> */}
+				</div>
+			</form>
+		</div>
+	);
+};
+
+export default RegisterPage;
