1da628347a5fe2885637cf7e5705aeb2b1b32723
ObinnaAka <33449354+ObinnaAka@users.noreply.github.com>
2020-07-18 18:53:34 -0500

    Corrected link

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -35,7 +35,7 @@ const httpLink = createHttpLink({
 });
 
 const wsLink = new WebSocketLink({
-	uri: `es://s6rbb7i554.execute-api.us-east-1.amazonaws.com/dev/graphql`,
+	uri: `ws://s6rbb7i554.execute-api.us-east-1.amazonaws.com/dev/graphql`,
 	options: {
 		reconnect: true,
 		connectionParams: {
